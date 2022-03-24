210b9159721e40cece0f617db4dea8ce3164044c
danielbrito <daniell-henrique@hotmail.com>
2022-03-23 23:54:00 -0300

    Make the first e2e test pass

---
+++ Test-Driven Development with React/bookish-react/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
